d27f22204c13125a2d10cf5b67c9f301eb8904bf
Cailie Skelton <cskelton707@gmail.com>
2020-04-24 17:03:06 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,12 +5,3 @@ def key_for_min_value(name_hash)
  
 end
 
-user_hash = {
-  :name => "Steve",
-  :location => "Idaho",
-  :email => "steve@stevemail.com"
-}
-name = user_hash[:name]
-user = ['Steve', 'Idaho']
-user_name = user[0]
-puts name, user_name
